715b3d188618ba69f2a43be5151091bbb3f6cf7a
maudrslt <81962494+maudrslt@users.noreply.github.com>
2021-12-06 16:02:50 +0100

    accueil

--- Accueil.php
+++ Accueil.php
@@ -1,54 +1,43 @@
-<?php require 'header.php'; ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ParisShopping</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="MiseEnPage1.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+<title>ParisShopping</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+ href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+<link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <nav class="navbar navbar-expand-md">
-        <a class="navbar-brand" href="Accueil.html">ParisShopping</a>
-        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="recherche"><input type="text" class="form-control" placeholder="Rechercher"></div>
-        <div class="collapse navbar-collapse" id="main-navigation">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="Accueil.html">Accueil</a></li>
-                <li class="nav-item"><a class="nav-link" href="ToutParcourir.html">Tout Parcourir</a></li>
-                <li class="nav-item"><a class="nav-link" href="Notifications.php">Notifications</a></li>
-                <li class="nav-item"><a class="nav-link" href="ChoixConnectionCompte.php">Mon compte</a></li>
-                <li class="nav-item"><a class="nav-link" href="Panier.php"><img src="Img/panier.jpg" alt="Panier" width="30 px"></a></li>
-            </ul>
-        </div>
-    </nav>
-    <section class="page-header header container-fluid">
-        <script type="text/javascript">
-    $(document).ready(function(){
-    $('.header').height($(window).height());
-    });
-    </script>
+ <a class="navbar-brand" href="Accueil.html">ParisShopping</a>
+ <a class="navbar-brand" href="Image/logo.png"></a><img src="Image/logo.png" alt="Logo" width="50 px"></a></li>
+ <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+ <span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="collapse navbar-collapse" id="main-navigation">
+ <ul class="navbar-nav">
+ <li class="nav-item"><a class="nav-link" href="Accueil.html">Accueil</a></li>
+ <li class="nav-item"><a class="nav-link" href="ToutParcourir.html">Tout Parcourir</a></li>
+ <li class="nav-item"><a class="nav-link" href="Notification.html">Notifications</a></li>
+ <li class="nav-item"><a class="nav-link" href="Image/panier.png"><img src="Image/panier.png" alt="Panier" width="30 px"></a></li>
+ <li class="nav-item"><a class="nav-link" href="MonCompte.html">Mon Compte</a></li>
+ </ul>
+ </div>
+</nav>
+<header class="page-header header container-fluid">
+    <script type="text/javascript">
+ $(document).ready(function(){
+ $('.header').height($(window).height());
+ });
+</script>
 
-     <div class="overlay"></div>
-     <div class="overlay"></div>
-     <div class="description">
-        <h1>Bienvenue sur ParisShopping !</h1>
+ <div class="overlay"></div>
+ <div class="description">
+        <h1>Bienvenue sur Paris Shopping !</h1>
         <p> ParisShopping est un site pour le magasinage en ligne pour le grand public. Ce site vous permet d'acheter des articles en vente sur le site.<br>
             <br>
             ParisShopping est ouvert à tous les Parisiens. Si vous avez un compte vendeur sur le site, vous pouvez mettre des articles en vente sur le site. Chaque article peut être vendu de différente manière :
@@ -57,9 +46,6 @@
                 <li> par négociation (il y a une discussion entre le vendeur et le client afin qu'ils se mettent d'accord sur un prix) </li>
                 <li> par meilleure offre (aux enchères) </li>
             </ul>
-            </p> 
-        <button class="btn btn-outline-secondary btn-lg"><a class="nav-link" href="ChoixConnectionCompte.php">Connexion</a></button>
-     </div>
-    </section>
-
-    <?php require 'footer.php'; ?>
+</header>
+</body>
+</html>
