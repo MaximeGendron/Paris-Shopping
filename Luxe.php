f2178ce50a8bd7c4650016147cb4a9b844aeb1cd
maudrslt <81962494+maudrslt@users.noreply.github.com>
2021-12-10 16:18:57 +0100

    tableau notif

--- Luxe.php
+++ Luxe.php
@@ -52,7 +52,7 @@ $panier = new panier($DB);
 
     <div class="overlay"></div>
     <div class="Titre">
-    <h2>Friperie</h2></div>
+    <h2>Luxe</h2></div>
 
     <div class="description2">
         
@@ -102,6 +102,7 @@ $panier = new panier($DB);
                     ?>
                         
                 </div>
+            </div>
 
             <div class="col-lg-4 col-md-4 col-sm-12">
 
